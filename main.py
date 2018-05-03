d70d5d926893f0b32f04a0e3e60ae575a114001c
qqlilichong@163.com <1314521aa>
2018-05-03 21:30:14 +0800

    test code.

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-import os
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
+html = urlopen('http://www.pythonscraping.com/pages/warandpeace.html')
+bsObj = BeautifulSoup(html, 'lxml')
+result = bsObj.findAll('span', {'class': 'red'})
+for item in result:
+    print(item)
