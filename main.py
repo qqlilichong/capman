41121c5db2e4772add2c3872cb0c2d1ad67121de
qqlilichong@163.com <1314521aa>
2018-05-29 22:57:14 +0800

    upgrade db.

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from captain.sp_javlib_search import *
 if __name__ == '__main__':
     path_root = os.path.join(os.path.dirname(__file__), 'javlib')
 
-    jurl = 'http://www.javlibrary.com/ja/vl_maker.php?m=aria'
-    jmaker = 'PREMIUM'
+    jurl = 'http://www.javlibrary.com/ja/vl_maker.php?m=aqra'
+    jmaker = 'MOODYZ'
     jfilter = ['EXNAME', 'SONE_TITLE']
     scrapy_javlib_maker(path_root, jurl, jmaker, jfilter)
 
