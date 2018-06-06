e28505fb0f2cbff87a1c480b4fe230a0c0ed137f
qqlilichong@163.com <1314521aa>
2018-06-07 05:34:52 +0800

    upgrade db.

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     path_root = os.path.join(os.path.dirname(__file__), 'javlib')
     jsite = 'http://www.javlibrary.com/ja/'
 
-    jurl = jsite + 'vl_maker.php?m=aqra'
-    jmaker = 'MOODYZ'
+    jurl = jsite + 'vl_maker.php?m=aq4q'
+    jmaker = 'IDEAPOCKET'
 
     jfilter = ['EXNAME', 'SONE_TITLE', 'AVOP_PASS']
     scrapy_javlib_maker(path_root, jurl, jmaker.upper(), jfilter)
