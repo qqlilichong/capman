bbd585e46e0e49f4117af28c3f71e134c5b7b0ae
qqlilichong@163.com <1314521aa>
2018-06-09 08:51:40 +0800

    upgrade db.

--- main.py
+++ main.py
@@ -8,12 +8,10 @@ from captain.sp_javlib_search import *
 if __name__ == '__main__':
     jpath = os.path.join(os.path.dirname(__file__), 'javlib')
     jsite = 'http://www.javlibrary.com/ja/'
-
-    jurl = jsite + 'vl_maker.php?m=aq4q'
-    jmaker = 'IDEAPOCKET'
-
     jfilter = ['EXNAME', 'SONE_TITLE', 'AVOP_PASS']
-    scrapy_javlib_maker(jpath, jurl, jmaker.upper(), jfilter)
+
+    jurl, jmaker = ('vl_maker.php?m=aria', 'PREMIUM')
+    scrapy_javlib_maker(jpath, jsite + jurl, jmaker.upper(), jfilter)
 
     print('capman bye!')
     exit()
