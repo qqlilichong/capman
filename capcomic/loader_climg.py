a5e9fa711ed55e47fb6dfeed29447ba43bd94a92
Ric <qqlilichong@163.com>
2019-02-15 13:00:36 +0800

    update.

---
+++ capcomic/loader_climg.py
@@ -0,0 +1,30 @@
+
+#######################################################################
+
+import os
+import sys
+import t_webtool
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print(r'.py data.txt dir_src dir_dst')
+        sys.exit(0)
+
+    datafile = sys.argv[1]
+    dir_src = sys.argv[2]
+    dir_dst = sys.argv[3]
+    data = [line.strip() for line in t_webtool.fget(datafile).decode(r'utf-8').split('\n') if line]
+
+    result = 0
+    t_webtool.fmkdir(dir_dst)
+    for file_src in [os.path.join(dir_src, f) for f in data]:
+        if not os.path.exists(file_src):
+            continue
+
+        file_dst = os.path.join(dir_dst, os.path.basename(file_src))
+        t_webtool.fset(file_dst, t_webtool.fget(file_src))
+        result += 1
+
+    print(r'result : %s' % result)
+
+#######################################################################
