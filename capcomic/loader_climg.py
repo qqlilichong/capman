5d7f7a757f85d700494d1be6a69a4805fd556970
qqlilichong@163.com <1314521aa>
2019-02-16 10:00:16 +0800

    update.

--- capcomic/loader_climg.py
+++ capcomic/loader_climg.py
@@ -6,13 +6,19 @@ import sys
 import t_webtool
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print(r'.py data.txt dir_src dir_dst')
+    argc = len(sys.argv)
+    if argc < 4:
+        print(r'.py data.txt dir_src dir_dst fixext[opt]')
         sys.exit(0)
 
     datafile = sys.argv[1]
     dir_src = sys.argv[2]
     dir_dst = sys.argv[3]
+
+    fixext = None
+    if argc >= 5:
+        fixext = sys.argv[4]
+
     data = [line.strip() for line in t_webtool.fget(datafile).decode(r'utf-8').split('\n') if line]
 
     result = 0
@@ -22,6 +28,11 @@ if __name__ == "__main__":
             continue
 
         file_dst = os.path.join(dir_dst, os.path.basename(file_src))
+        if fixext:
+            fl = file_dst.split(r'.')
+            fl.insert(-1, fixext)
+            file_dst = r'.'.join(fl)
+
         t_webtool.fset(file_dst, t_webtool.fget(file_src))
         result += 1
 
