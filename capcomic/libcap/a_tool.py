ede57cbf004faf99bacbb7f1af1b90d1ffcba079
ric <qqlilichong@163.com>
2019-10-27 23:29:06 +0800

    update.

--- capcomic/libcap/a_tool.py
+++ capcomic/libcap/a_tool.py
@@ -19,8 +19,6 @@ async def tmr(*tasks):
 #######################################################################################################
 
 def tloop(task):
-    if sys.platform == r'win32':
-        asyncio.set_event_loop(asyncio.ProactorEventLoop())
     loop = asyncio.get_event_loop()
     loop.run_until_complete(task)
     loop.run_until_complete(asyncio.sleep(1))
